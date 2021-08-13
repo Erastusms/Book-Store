04a48209cd863b7abef90393abce4d71a97a15db
Septian7 <septian.ahmad7@gmail.com>
2021-08-13 15:16:38 +0700

    shopping cart

--- server/routes/shopping_cart.js
+++ server/routes/shopping_cart.js
@@ -1,7 +1,11 @@
 const cartRoute = require('express').Router();
 const cartController = require('../controllers/cartController')
 
-
+cartRoute.get('/',cartController.show)
+cartRoute.get('/:id',cartController.showById)
+cartRoute.post('/create',cartController.create)
+cartRoute.put('/update/:id',cartController.update)
+cartRoute.delete('/delete/:id',cartController.delete)
 
 
 module.exports = cartRoute
